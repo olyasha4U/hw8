74dbf8423b4749a30c11ee0e8e517f1d45952903
olyasha4U <lybo.km15@gmail.com>
2025-11-16 17:06:38 +0200

    commit8

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import logging
 # logging.basicConfig(level=logging.INFO, filename="year.log", filemode="w", format="Year-Month-Day :  %(message)s")
 # logging.info("2025:11:Saturday")
 
-# 2
+# 3
 # def login(username, password):
 #     correct_username = "Olga"
 #     correct_password = "12345"
@@ -16,14 +16,26 @@ import logging
 #
 # login("Olga", "12345")
 # login("user", "54321")
-3
-def check_age(age):
-    age_ok =18
-    try:
-        assert age >= age_ok
-        print("Ви можете використовувати цей сервіс")
-    except:
-        print("Вам має бути 18 років або більше")
+# 4
+# def check_age(age):
+#     age_ok =18
+#     try:
+#         assert age >= age_ok
+#         print("Ви можете використовувати цей сервіс")
+#     except:
+#         print("Вам має бути 18 років або більше")
+#
+# check_age(13)
+# check_age(19)
+5
+def process_list (input_list):
 
-check_age(13)
-check_age(19)
+    try:
+        assert len(input_list) >= 3
+        print(f"Список містить {len(input_list)}  елементів")
+    except:
+        print("Список повинен містити принаймні 3 елементи")
+
+process_list(["a"])
+process_list(["abc", "a","b", 3])
+process_list(["abc", "a","b"])
